d43f309a0de719c0b73a5189ddc1800bf78665f9
donate <150584801+niche-markets@users.noreply.github.com>
2025-05-29 02:10:15 +0000

    fix: add

--- netlify/edge-functions/blog-proxy.ts
+++ netlify/edge-functions/blog-proxy.ts
@@ -3,7 +3,7 @@ import { Context } from "https://edge.netlify.com";
 export default async (request: Request, context: Context) => {
   const blogUrl = new URL(request.url);
   const path = blogUrl.pathname.replace(/^\/blog/, "");
-  const proxyUrl = `https://astro-sanity-blog-test.netlify.app${path}${blogUrl.search}`;
+  const proxyUrl = `https://astro-sanity-blog-test.netlify.app`;
 
   const response = await fetch(proxyUrl, {
     headers: request.headers,
